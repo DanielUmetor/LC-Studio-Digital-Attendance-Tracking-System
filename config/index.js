4c04725f8c2a7874a72668a924427387002b4796
AChar1f <acharif41@gmail.com>
2025-01-09 15:18:41 +0200

    express app

--- config/index.js
+++ config/index.js
@@ -1,10 +1,11 @@
 import { createPool } from "mysql2";
+import 'dotenv/config'
 
 let connection = createPool ({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PWD,
-    database: process.env.DB_DBNAME,
+    database: process.env.DB_NAME,
     multipleStatements: true,
     connectionLimit: 100
 })
--- index.js
+++ index.js
@@ -1,3 +1,39 @@
-const create app = express()
+import path from 'path'
+import cors from 'cors'
 
+
+const app = express()
 const port = +process.env.PORT || 4000
+
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Credentials", "true")
+    res.header("Access-Control-Allow-Methods", "*")
+    res.header("Access-Control-Request-Methods", "*")
+    res.header("Access-Control-Allow-Headers", "*")
+    res.header("Access-Control-Expose-Headers", "Authorization")
+    next()
+  })
+  app.use(
+    express.static("./static"),
+    express.json(),
+    express.urlencoded({
+      extended: true
+      }),
+      cors()
+  )
+
+app.get('^/$|/home', (req, res) => {
+    res.status(200).sendFile(path.resolve('./static/html/index.html'))
+})
+app.get('*', (req, res) => {
+    res.json({
+        status: 404,
+        msg: '❌ Resource not found.'
+    })
+})
+app.use(errorHandling)
+app.listen(port, () => {
+    console.log(`Live on port: ${port}`)
+})
